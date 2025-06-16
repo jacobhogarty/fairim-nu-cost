0de5ad74037ec4ec35938a4730a3e3d0d82cb2f9
Cabin320 <jacob.hogarty@yahoo.com>
2025-06-16 19:21:00 +0100

    Slight modification to graph_generator.py

--- data/synthetic/generator/graph_generator.py
+++ data/synthetic/generator/graph_generator.py
@@ -161,7 +161,7 @@ class GraphGenerator(ABC):
 
         if node_labels:
             label_offset = {node: (pos[node][0], pos[node][1] - 0.05) for node in self.graph.nodes()}
-            cost_labels = {node: f"£{self.node_costs.get(node, '?')}" for node in self.graph.nodes()}
+            cost_labels = {node: f"{self.node_costs.get(node, '?')}%.2f" for node in self.graph.nodes()}
             draw_networkx_labels(
                 self.graph,
                 pos=label_offset,
