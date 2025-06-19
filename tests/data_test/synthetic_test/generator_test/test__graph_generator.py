25c8f452ee6d4c5da4c72aa6c807a389a732979f
Cabin320 <jacob.hogarty@yahoo.com>
2025-06-20 00:51:37 +0100

    Fixing bug with random change due tp PyCharm bug

--- tests/data_test/synthetic_test/generator_test/test__graph_generator.py
+++ tests/data_test/synthetic_test/generator_test/test__graph_generator.py
@@ -23,7 +23,7 @@ def test__graph_generator(generate_complete_graph, nodes):
         n=nodes,
     )
 
-    assert len(generate_complete_graph.ba_100_graph) == nodes
+    assert len(generate_complete_graph.graph) == nodes
 
     expected_edges = nodes * (nodes - 1) // 2
-    assert len(generate_complete_graph.ba_100_graph.edges) == expected_edges
+    assert len(generate_complete_graph.graph.edges) == expected_edges
